bf8d3683438ea7f5c287eb7918b17bd6712eebb8
Keith Duncan <keith.duncan@github.com>
2015-12-14 18:50:55 +1100

    Add SCOTTY_ENV environment variable parsing

---
+++ src/Environment.hs
@@ -0,0 +1,16 @@
+module Environment (
+  Environment(..),
+  getEnvironment,
+) where
+
+import System.Environment (lookupEnv)
+
+data Environment = Development | Production
+
+instance Read Environment where
+  readsPrec _ e = case e of
+                   "development" -> [(Development, "")]
+                   "production"  -> [(Production, "")]
+
+getEnvironment :: IO Environment
+getEnvironment = maybe Production read <$> lookupEnv "SCOTTY_ENV"
