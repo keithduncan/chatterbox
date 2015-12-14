7019857dc46cc029c9f0ea208c99fff647156c13
Keith Duncan <keith.duncan@github.com>
2015-12-14 22:07:27 +1100

    Stub out a workqueue API

---
+++ src/Model/Message.hs
@@ -0,0 +1,9 @@
+module Model.Message (
+  Message,
+  message,
+) where
+
+data Message = Message
+
+message :: Message
+message = Message
--- src/Workqueue.hs
+++ src/Workqueue.hs
@@ -4,7 +4,13 @@ module Workqueue (
   getWorkqueue,
 ) where
 
+import Model.Message
+import Model.Subscription (Topic)
+
 data Workqueue = Workqueue
 
 getWorkqueue :: IO Workqueue
 getWorkqueue = return Workqueue
+
+enqueueSay :: Workqueue -> Topic -> Message -> IO ()
+enqueueSay _ _ _ = return ()
