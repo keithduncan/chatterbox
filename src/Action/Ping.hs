9ee749d82e2dc086c6c5d7ba1ea769708831c1f8
Keith Duncan <keith.duncan@github.com>
2015-12-14 16:52:16 +1100

    Fix the ping action

--- src/Action/Ping.hs
+++ src/Action/Ping.hs
@@ -2,17 +2,21 @@ module Action.Ping (
   ping,
 ) where
 
-import System.Time
+import Data.Time.Clock.POSIX
 
 import Control.Monad
+import Control.Monad.IO.Class
 
 import Web.Scotty
+import Network.HTTP.Types
 
 import Data.Map
 
 ping :: ActionM ()
 ping = do
-  (TOD sec _) <- liftIO getClockTime
+  sec <- liftIO getPOSIXTime
 
-  let status = fromList [("status", "ok"), ("now", show sec)] :: Map String String
-  status ok200 >> json status
+  let ping = fromList [("status", "ok"), ("now", show sec)] :: Map String String
+
+  status ok200
+  json ping
