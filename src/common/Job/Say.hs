11c0a6d00b41172728d437ad0a49d0e3fa4b7687
Keith Duncan <keith.duncan@github.com>
2015-12-16 10:43:00 +1100

    Make SayJob an instance of Job

--- src/common/Job/Say.hs
+++ src/common/Job/Say.hs
@@ -1,15 +1,25 @@
 {-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
 
 module Job.Say (
 ) where
 
+import System.Hworker
+
+import Control.Monad (void)
+
 import Model.Message
 import Model.Subscription (Topic)
 
-import Data.Aeson
-import GHC.Generics
+import Data.Aeson (FromJSON, ToJSON)
+import GHC.Generics (Generic)
 
 data SayJob = Say Topic Message deriving (Show, Generic)
 
 instance ToJSON SayJob
 instance FromJSON SayJob
+
+instance Job () SayJob where
+  job () (Say topic message) = do
+    void $ print (show message ++ " -> " ++ topic)
+    return Success
