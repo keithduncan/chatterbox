1a835ff4a29db4229854b788b2f0a18b02e423cc
Keith Duncan <keith.duncan@github.com>
2015-12-18 19:18:02 +1100

    Shorten the strip functions

--- src/common/Workqueue.hs
+++ src/common/Workqueue.hs
@@ -18,6 +18,8 @@ import qualified Database.Redis as R
 import Network.URI
 
 import Data.ByteString.Char8 (pack)
+import Data.List (stripPrefix)
+import Data.Maybe (fromMaybe)
 
 import Job.Say
 import Job.Expire
@@ -68,17 +70,8 @@ redisConnectionInfo (URI "redis:" (Just (URIAuth auth regname port)) path _ _) =
                    then R.connectDatabase R.defaultConnectInfo
                    else read db
 
-    stripLeading :: Eq a => a -> [a] -> [a]
-    stripLeading x [] = []
-    stripLeading x xs@(xsHead:xsTail)
-      | x == xsHead = xsTail
-      | otherwise   = xs
-
-    stripTrailing :: a -> [a] -> [a]
-    stripTrailing x [] = []
-    stripTrailing x xs = let init' = init xs
-                             tail' = tail xs
-                          in init'
+    stripLeading x xs = fromMaybe xs $ stripPrefix [x] xs
+    stripTrailing x xs = maybe xs reverse $ stripPrefix [x] (reverse xs)
 redisConnectionInfo uri = error $ "invalid URI " ++ show uri
 
 getSayHworker :: RedisConnection -> IO SayWorker
